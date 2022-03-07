747a12155980aeb6211feb84450de530fa8ab359
reyrarnfredur <a.reyr@gmx.us>
2022-03-07 21:14:26 +0100

    Add stat list to character component

--- Assets/Game/Scripts/Core/Characters/Character.cs
+++ Assets/Game/Scripts/Core/Characters/Character.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using Core.Stats;
 
 namespace Core.Characters {
 
     public class Character : MonoBehaviour
     {
         
-        
+        public StatList stats;
 
     }
 
