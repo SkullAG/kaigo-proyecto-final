0fb10d7c3b247859a19e2cac2f6b92e1be9008ee
reyrarnfredur <a.reyr@gmx.us>
2022-03-07 21:10:13 +0100

    Add resource pool script (health and mana)

---
+++ Assets/Game/Scripts/Core/Stats/Resource Pools/Resource.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Stats {
+
+    [System.Serializable]
+    public class Resource
+    {
+
+        public System.Action<int> onValueUpdated = delegate {};
+        public System.Action<int> onValueMax = delegate {};
+        public System.Action<int> onValueMin = delegate {};
+
+        [SerializeField]
+        protected int _currentValue;
+
+        [SerializeField]
+        protected int _maxValue;
+
+        public int value {
+            get => GetValue();
+            set => SetValue(value);
+        }
+
+        public void SetValue(int value) {
+
+            _currentValue = Mathf.Clamp(value, 0, _maxValue);
+            onValueUpdated(_currentValue);
+
+            if(value == _maxValue) {
+                onValueMax(value);
+            } else if (value == 0) {
+                onValueMin(value);
+            }
+
+        }
+
+        public int GetValue() {
+            return _maxValue;
+        }
+
+    }
+
+}
+
