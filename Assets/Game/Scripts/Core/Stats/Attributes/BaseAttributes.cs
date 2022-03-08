265c3b37bb7f3dfbfe8ed69b6b290e35db51f143
reyrarnfredur <a.reyr@gmx.us>
2022-03-08 12:13:41 +0100

    Revert "Rename base attributes script to attributes"
    This reverts commit f406adb08e3410be57b256c26382d59158deb37c.

---
+++ Assets/Game/Scripts/Core/Stats/Attributes/BaseAttributes.cs
@@ -0,0 +1,19 @@
+using NaughtyAttributes;
+
+namespace Core.Stats {
+
+    [System.Serializable]
+    public class BaseAttributes {
+
+        public Attribute strength;
+        public Attribute wisdom;
+        public Attribute constitution;
+        public Attribute faith;
+        public Attribute accuracy;
+        public Attribute evasion;
+        public Attribute agility;
+
+    }
+
+}
+
