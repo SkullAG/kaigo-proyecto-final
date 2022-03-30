6c8421d40979a2028c77d41966480b45fad20103
Laia2G <laiagarciagonzalez18@gmail.com>
2022-03-30 14:27:29 +0200

    Update list inventory

--- Assets/Game/Scenes/Testing/Laia/Scripts/ButtonConstructor.cs
+++ Assets/Game/Scenes/Testing/Laia/Scripts/ButtonConstructor.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ButtonConstructor : MonoBehaviour
 {
-    public GameObject buttons;
+    //public GameObject buttons;
     public GameObject _button;
+    public Inventory _inventory;
 
-    public void MakeButton()
+    public void OnEnable()
     {
-        Instantiate(_button, transform.position, transform.rotation);
+        _inventory.SendOnInventoryChange.AddListener(UpdateList);
+    }
+
+    [SerializeField]
+    public List<GameObject> buttonslist = new List<GameObject>();
+    public void UpdateList(Dictionary<string, Inventory.Casilla> huecos)
+    {
+        GameObject but = Instantiate(_button, transform.position, transform.rotation);
+
+        but.transform.parent = transform;
+
+        buttonslist.Add(but);
     }
 }
