507de0b9bc8959e510478e284aed4ee3b8d25195
reyrarnfredur <a.reyr@gmx.us>
2022-04-08 22:50:03 +0200

    Fix animation phase trying to play without animator haha

--- Assets/Game/Scripts/Core/Actions/Phases/Multi-purpose/PlayAnimation.cs
+++ Assets/Game/Scripts/Core/Actions/Phases/Multi-purpose/PlayAnimation.cs
@@ -26,8 +26,12 @@ public class PlayAnimation : ActionPhase
 
             _animator = actor.gameObject.GetComponentInChildren<Animator>();
 
-            Debug.Log("Playing animation: " + _animationStateName);
-            _animator.Play(_animationStateName);
+            if(_animator != null) {
+
+                Debug.Log("Playing animation: " + _animationStateName);
+                _animator.Play(_animationStateName);
+
+            }
 
         }
 
@@ -53,13 +57,17 @@ public class PlayAnimation : ActionPhase
 
             // Trigger phase end when animation ends
 
-            if(_animator.GetCurrentAnimatorStateInfo(0).IsName(_animationStateName)) {
+            if(_animator != null) {
 
-                var _info = _animator.GetCurrentAnimatorStateInfo(0);
+                if(_animator.GetCurrentAnimatorStateInfo(0).IsName(_animationStateName)) {
 
-                if( _info.normalizedTime >= 1 ) {
+                    var _info = _animator.GetCurrentAnimatorStateInfo(0);
 
-                    End();
+                    if( _info.normalizedTime >= 1 ) {
+
+                        End();
+
+                    }
 
                 }
 
