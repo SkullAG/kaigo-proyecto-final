b032d967c0ae646e576a7a649d6c101a91f47fb6
reyrarnfredur <a.reyr@gmx.us>
2022-05-01 17:19:40 +0200

    Fix action queue not using action timer correctly

--- Assets/Game/Scripts/Core/Actions/ActionQueue.cs
+++ Assets/Game/Scripts/Core/Actions/ActionQueue.cs
@@ -10,8 +10,10 @@ namespace Core.Actions
         
         [SerializeField] private bool _updating = true;
         [SerializeField] private bool _busy = false;
-        [SerializeField] private float _actionTimer = 0;
-        [SerializeField] private float _actionSpeed = 1;
+        [SerializeField] private bool _ready = true;
+
+        [SerializeField, Range(0, 1)] private float _actionSpeed = 1; // 1 means no action wait
+        [SerializeField, ReadOnly] private float _actionTimer = 0;
 
         [SerializeField] private GameAction _queuedAction;
 
@@ -19,32 +21,47 @@ namespace Core.Actions
 
         [SerializeField, ReadOnly, Expandable] private GameAction _currentAction;
 
+        public bool isPerformingAction => _busy;
+        public bool isReady => _ready;
+
+        public float actionSpeed { set => _actionSpeed = Mathf.Clamp01(value); }
+
         private void FixedUpdate() {
 
             if(_updating) {
 
-                // Update current action processing
-                _currentAction?.Update();
+                if(!_busy) {
 
-                if( _actionTimer >= 1.0f ) {
+                    _actionTimer -= Time.deltaTime * _actionSpeed;
 
-                    if(!_busy && _queuedAction != null) {
+                    // If the action timer is 0, character can execute actions
+                    if(_actionTimer <= 0) {
 
-                        _currentAction = _queuedAction;
-                        _queuedAction = null;
-                        _currentAction.Execute();
+                        _actionTimer = 0;
+                        _ready = true;
 
-                        _currentAction.onActionEnd += OnActionEnd;
+                        // If there's a queued action, execute it!
+                        if(_queuedAction != null) {
 
-                        _busy = true;
+                            _currentAction = _queuedAction;
+                            _queuedAction = null;
+                            _currentAction.Execute();
+
+                            _currentAction.onActionEnd += OnActionEnd;
+
+                            _busy = true;
+                            _ready = false;
+
+                        }
 
                     }
 
                 } else {
 
-                    _actionTimer += Time.deltaTime * _actionSpeed;
+                    // Update current action processing if busy
+                    _currentAction?.Update();
 
-                }
+                }       
 
             } else {
 
@@ -55,10 +72,11 @@ namespace Core.Actions
         }
 
         private void OnActionEnd() {
-            
-            _actionTimer = 0;
 
             _currentAction.onActionEnd -= OnActionEnd; // Stop listening action end event
+
+            // When action ends, timer resets
+            _actionTimer = 1;
 
             _busy = false;
 
