259281ee672db074d37883eb011c6dbe97bb8fb2
reyrarnfredur <a.reyr@gmx.us>
2022-05-02 20:21:56 +0200

    Add timer to disable battle log when there are no logs comming

--- Assets/Game/Scripts/Core/UI/BattleLog.cs
+++ Assets/Game/Scripts/Core/UI/BattleLog.cs
@@ -7,11 +7,15 @@ using TMPro;
 public class BattleLog : Singleton<BattleLog>
 {
     
+    [SerializeField] private RectTransform _container;
     [SerializeField] private int _maxLogs;
     [SerializeField] private string _testText;
     [SerializeField] private string _format = "<b>·</b> {0}";
+    [SerializeField] private float _timeToClose = 5;
 
     private List<string> _logs = new List<string>();
+    private float _timer;
+    private bool _enabled;
 
     private ScrollRect _scrollRect;
     private TextMeshProUGUI _textMesh;
@@ -23,7 +27,38 @@ public class BattleLog : Singleton<BattleLog>
 
     }
 
+    public override void OnEnable() {
+
+        base.OnEnable();
+
+        _container.gameObject.SetActive(false);
+        _enabled = false;
+
+    }
+
+    private void Update() {
+
+        if(_enabled) {
+
+            _timer -= Time.deltaTime;
+
+            if(_timer < 0) {
+
+                _timer = 0; // Reset to 0 because why nah gg
+                _container.gameObject.SetActive(false);
+                _enabled = false;
+
+            }
+
+        }
+
+    }
+
     public void WriteLine(string text) {
+
+        _container.gameObject.SetActive(true);
+        _enabled = true;
+        _timer = _timeToClose; 
 
         _logs.Add(string.Format(_format, text));
 
