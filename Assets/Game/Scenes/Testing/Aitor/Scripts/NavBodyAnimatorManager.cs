0d755caf0e67e15ca205043cbfdbefaad5eea875
reyrarnfredur <a.reyr@gmx.us>
2022-05-15 22:32:57 +0200

    Pass smoothed movement factor to character's animator

--- Assets/Game/Scenes/Testing/Aitor/Scripts/NavBodyAnimatorManager.cs
+++ Assets/Game/Scenes/Testing/Aitor/Scripts/NavBodyAnimatorManager.cs
@@ -8,18 +8,33 @@ public class NavBodyAnimatorManager : MonoBehaviour
     NavBodySistem agent;
     public Animator animator;
 
+    public float movementFactorDelta = 1;
+    public float rotationFactorDelta = 1;
+
+    float _movementFactor;
+    float _rotationFactor;
+
+    float _startupTime;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavBodySistem>();
 
         if (!animator) animator = GetComponent<Animator>();
+
+        _startupTime = Time.realtimeSinceStartup;
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        animator.SetFloat("movementFactor", agent.movementFactor);
-        animator.SetFloat("rotationFactor", agent.rotationFactor);
+    void Update() {
+
+        _movementFactor = Mathf.MoveTowards(_movementFactor, agent.movementFactor, movementFactorDelta * Time.deltaTime);
+        _rotationFactor = Mathf.MoveTowards(_rotationFactor, agent.rotationFactor, rotationFactorDelta * Time.deltaTime);;
+
+        animator.SetFloat("movementFactor", _movementFactor);
+        animator.SetFloat("rotationFactor", _rotationFactor);
+        
     }
 }
