2720a63d8807dec5d90d0bdd191704fe925d574b
SkullAG <aitormine12@gmail.com>
2022-05-17 21:32:20 +0200

    Death now killing

--- Assets/Game/Scripts/Core/Stats/StatList.cs
+++ Assets/Game/Scripts/Core/Stats/StatList.cs
@@ -6,109 +6,109 @@ using Core.States;
 
 namespace Core.Stats {
 
-    public class StatList : MonoBehaviour
-    {
+	public class StatList : MonoBehaviour
+	{
 
-        [SerializeField] private State deathState;
-        
-        // Resource stats
-        [BoxGroup("Resource")] public Resource healthPoints;
-        [BoxGroup("Resource")] public Resource actionPoints;
+		[SerializeField] private State deathState;
+		
+		// Resource stats
+		[BoxGroup("Resource")] public Resource healthPoints;
+		[BoxGroup("Resource")] public Resource actionPoints;
 
-        // Base stats
-        [BoxGroup("Base")] public Bravery bravery;
-        [BoxGroup("Base")] public Constitution constitution;
-        [BoxGroup("Base")] public Vitality vitality;
-        [BoxGroup("Base")] public Determination determination;
-        [BoxGroup("Base")] public Agility agility;
+		// Base stats
+		[BoxGroup("Base")] public Bravery bravery;
+		[BoxGroup("Base")] public Constitution constitution;
+		[BoxGroup("Base")] public Vitality vitality;
+		[BoxGroup("Base")] public Determination determination;
+		[BoxGroup("Base")] public Agility agility;
 
-        // Affinity stats
-        [BoxGroup("Affinities")] public Affinities.Affinities affinity;
+		// Affinity stats
+		[BoxGroup("Affinities")] public Affinities.Affinities affinity;
 
-        private Character _user;
+		private Character _user;
 
-        private void Awake() {
+		private void Awake() {
 
-            _user = GetComponent<Character>();
+			_user = GetComponent<Character>();
 
-            healthPoints.value = healthPoints.max;
-            actionPoints.value = actionPoints.max;
+			healthPoints.value = healthPoints.max;
+			actionPoints.value = actionPoints.max;
 
-            vitality.onValueChanged += OnAttributeValueChanged;
-            determination.onValueChanged += OnAttributeValueChanged;
+			vitality.onValueChanged += OnAttributeValueChanged;
+			determination.onValueChanged += OnAttributeValueChanged;
 
-            healthPoints.onValueUpdated += OnHealthUpdated;
-            actionPoints.onValueUpdated += OnActionPointsUpdated;
+			healthPoints.onValueUpdated += OnHealthUpdated;
+			actionPoints.onValueUpdated += OnActionPointsUpdated;
 
-            UpdateValues();
+			UpdateValues();
 
-        }
+		}
 
-        private void OnValidate() {
+		private void OnValidate() {
 
-            if(_user) UpdateValues();
+			if(_user) UpdateValues();
 
-        }
+		}
 
-        private void OnAttributeValueChanged(float value) {
+		private void OnAttributeValueChanged(float value) {
 
-            UpdateValues();
-            
-        }
+			UpdateValues();
+			
+		}
 
-        public void UpdateValues() {
+		public void UpdateValues() {
 
-            healthPoints.max = Mathf.RoundToInt(vitality.CalculateMaximumHealth());
-            actionPoints.max = Mathf.RoundToInt(determination.CalculateMaxActionPoints());
+			healthPoints.max = Mathf.RoundToInt(vitality.CalculateMaximumHealth());
+			actionPoints.max = Mathf.RoundToInt(determination.CalculateMaxActionPoints());
 
-            _user.queue.actionSpeed = agility.CalculateActionSpeed();
+			_user.queue.actionSpeed = agility.CalculateActionSpeed();
 
-            bravery.CalculateModifiers();
-            constitution.CalculateModifiers();
-            vitality.CalculateModifiers();
-            determination.CalculateModifiers();
-            agility.CalculateModifiers();
+			bravery.CalculateModifiers();
+			constitution.CalculateModifiers();
+			vitality.CalculateModifiers();
+			determination.CalculateModifiers();
+			agility.CalculateModifiers();
 
-        }
+		}
 
-        public void OnHealthUpdated(int lastValue, int currentValue) {
+		public void OnHealthUpdated(int lastValue, int currentValue) {
 
-            int _d = currentValue - lastValue;
+			int _d = currentValue - lastValue;
 
-            if(_d < 0) {
+			if(_d < 0) {
 
-                BattleLog.current.WriteLine(string.Format(BattleLogFormats.DAMAGE_RECEIVED, _user.name, Mathf.Abs(_d)));
+				BattleLog.current.WriteLine(string.Format(BattleLogFormats.DAMAGE_RECEIVED, _user.name, Mathf.Abs(_d)));
 
-                if(currentValue <= 0) { // Add death when health is 0
+				if(currentValue <= 0) { // Add death when health is 0
+					_user.states.AddState(deathState, Mathf.Infinity, 1);
+				}
 
-                    Debug.Log("CHaracter died!");
+			} else if (_d > 0) {
 
-                    _user.states.AddState(deathState, Mathf.Infinity, 1);
+				BattleLog.current.WriteLine(string.Format(BattleLogFormats.HEALTH_RECOVERED, _user.name, Mathf.Abs(_d)));
 
-                }
+				if (lastValue <= 0) {
+					_user.states.RemoveState(deathState);
+				}
 
-            } else if (_d > 0) {
+			}
 
-                BattleLog.current.WriteLine(string.Format(BattleLogFormats.HEALTH_RECOVERED, _user.name, Mathf.Abs(_d)));
+		}
 
-            }
+		public void OnActionPointsUpdated(int lastValue, int currentValue) {
 
-        }
+			int _d = currentValue - lastValue;
 
-        public void OnActionPointsUpdated(int lastValue, int currentValue) {
+			// Don't log action points lost :S
+			if (_d > 0) {
 
-            int _d = currentValue - lastValue;
+				BattleLog.current.WriteLine(string.Format(BattleLogFormats.ACTIONP_RECOVERED, _user.name, _d));
 
-            // Don't log action points lost :S
-            if (_d > 0) {
+			}
 
-                BattleLog.current.WriteLine(string.Format(BattleLogFormats.ACTIONP_RECOVERED, _user.name, _d));
+		}
 
-            }
-
-        }
-
-    }
+	}
 
 }
  
